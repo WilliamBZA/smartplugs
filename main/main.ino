542cbff5f3ac26d502c02eee373b786d1805f0ad
williambza <william.brander@gmail.com>
2023-07-20 14:21:42 +0200

    Work with second relay

--- main/main.ino
+++ main/main.ino
@@ -14,6 +14,14 @@ WiFiManagerParameter device_name_parameter("devicename", "Device Name", "", 40);
 AsyncWebServer server(80);
 
 String deviceName = "UnknownDevice";
+int relay1Pin = 13;
+bool relay1State = false;
+String relay1Name = "Switch 1";
+
+bool relay2Enabled = false;
+int relay2Pin = 11;
+bool relay2State = false;
+String relay2Name = "Switch 2";
 
 void connectToWifi() {
   wifiManager.addParameter(&device_name_parameter);
@@ -75,12 +83,40 @@ void configureOTA() {
   ArduinoOTA.begin();
 }
 
-String valueProcessor(const String& var)
-{
+String getSettings() {
+  String settings = "{\"devicename\": \"";
+  settings += deviceName;
+  settings += "\", \"relay1Name\": \"";
+  settings += relay1Name;
+  settings += "\", \"relay1State\": \"";
+  settings += relay1State;
+  settings += "\", \"relay1Pin\": \"";
+  settings += relay1Pin;
+  settings += "\", \"hasSecondRelay\": \"";
+  settings += relay2Enabled;
+
+  settings += "\", \"relay2Name\": \"";
+  settings += relay2Name;
+  settings += "\", \"relay2State\": \"";
+  settings += relay2State;
+  settings += "\", \"relay2Pin\": \"";
+  settings += relay2Pin;
+
+  settings += "\"}";
+  return settings;
+}
+
+String valueProcessor(const String& var) {
   Serial.print("Var: "); Serial.println(var);
   
-  if(var == "DEVICE_NAME")
+  if (var == "DEVICE_NAME") {
     return deviceName;
+  }
+
+  if (var == "START_SETTINGS") {
+    return getSettings();
+  }
+
   return var;
 }
 
@@ -98,16 +134,22 @@ void configureUrlRoutes() {
 
   server.on("/api/currentsettings", HTTP_GET, [](AsyncWebServerRequest *request) {
     Serial.print("Sending settings..."); Serial.println("");
-    request->send(200, "text/json", "{\"devicename\": \"" + deviceName + "\"}");
+    request->send(200, "text/json", getSettings());
   });
   
   server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){
+    Serial.printf("[REQUEST]\t%s\r\n", (const char*)data);
+    Serial.print("URL: "); Serial.println(request->url());
+    
     if (request->url() == "/api/savesettings") {
       saveSettings(request, data);
       request->send(200, "text/json", "OK");
 
       delay(500);
       ESP.restart();
+    } else if (request->url() == "/api/togglerelay") {
+      toggleRelay(request, data);
+      request->send(200, "text/json", "OK");
     } else {
       request->send(404);
     }
@@ -118,30 +160,87 @@ void configureUrlRoutes() {
   });
 }
 
-void saveSettings(AsyncWebServerRequest *request, uint8_t *data) {
-  Serial.println("Saving settings...");
-
-  Serial.printf("[REQUEST]\t%s\r\n", (const char*)data);
+void toggleRelay(AsyncWebServerRequest *request, uint8_t *data) {
+  Serial.println("Toggling relay...");
 
   StaticJsonDocument<256> doc;
   deserializeJson(doc, data);
+
+  int relayNum = doc["relay"];
+  int shouldBeOn = !doc["state"];
+
+  if (relayNum == 1) {
+    Serial.print("Toggling PIN: "); Serial.print(relay1Pin); Serial.print(" to state: "); Serial.println(shouldBeOn);
+    digitalWrite(relay1Pin, shouldBeOn);
+    relay1State = !shouldBeOn;
+  } else if (relayNum == 2) {
+    Serial.print("Toggling PIN: "); Serial.print(relay2Pin); Serial.print(" to state: "); Serial.println(shouldBeOn);
+    digitalWrite(relay2Pin, shouldBeOn);
+    relay2State = !shouldBeOn;
+  }
+}
+
+void saveSettings(AsyncWebServerRequest *request, uint8_t *data) {
   Serial.println("Saving settings...");
+
+  StaticJsonDocument<256> doc;
+
+  DeserializationError error = deserializeJson(doc, (const char *)data, request->contentLength());
   
-  char name[64];
-  strcpy(name, doc["devicename"]);
-  deviceName = name;
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+  
+  const char* devicename = doc["devicename"]; // "Pool pump"
+  int relay1pin = doc["relay1pin"]; // 13
+  const char* relay1name = doc["relay1name"]; // "asdasd"
+  bool relay2enabled = doc["relay2enabled"]; // true
+  int relay2pin = doc["relay2pin"]; // 13
+  const char* relay2name = doc["relay2name"]; // "asdasd"
+
+  deviceName = devicename;
+  relay1Pin = relay1pin;
+  relay1Name = relay1name;
+
+  // restart if relay 2 was disabled but is now enabled
+  bool mustRestart = relay2enabled != relay2Enabled && relay2enabled;
+
+  relay2Enabled = relay2enabled;
+  if (relay2Enabled) {
+    relay2Pin = relay2pin;
+    relay2Name = relay2name;
+  }
 
   Serial.print("Device name: "); Serial.println(deviceName);
+  Serial.print("Relay 1: "); Serial.print(relay1Name); Serial.print(" on pin: "); Serial.println(relay1Pin);
+
+  Serial.print("Relay 2 enabled: "); Serial.println(relay2Enabled);
+  if (relay2Enabled) {
+    Serial.print("Relay 2: "); Serial.print(relay2Name); Serial.print(" on pin: "); Serial.println(relay2Pin);
+  }
   
   File settingsFile = SPIFFS.open("/settings.json", "w");
-  StaticJsonDocument<256> settingsDoc;
+  StaticJsonDocument<1024> settingsDoc;
   settingsDoc["devicename"] = deviceName;
+  
+  settingsDoc["relay1pin"] = relay1Pin;
+  settingsDoc["relay1Name"] = relay1Name;
+
+  settingsDoc["relay2Enabled"] = relay2Enabled;
+  settingsDoc["relay2pin"] = relay2Pin;
+  settingsDoc["relay2Name"] = relay2Name;
 
   if (serializeJson(settingsDoc, settingsFile) == 0) {
     Serial.println("Failed to write to file");
   }
 
   settingsFile.close();
+
+  if (mustRestart) {
+    ESP.restart();
+  }
 }
 
 void loadDeviceSettings() {
@@ -152,20 +251,39 @@ void loadDeviceSettings() {
     return;
   }
 
-  StaticJsonDocument<512> settingsDoc;
-  DeserializationError error = deserializeJson(settingsDoc, settingsFile);
+  StaticJsonDocument<384> doc;
 
+  DeserializationError error = deserializeJson(doc, settingsFile);
   if (error) {
-    Serial.print("Error parsing JSON: "); Serial.println(error.c_str());
-    deviceName = "UnknownDevice";
-    settingsFile.close();
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
     return;
   }
+  
+  const char* devicename = doc["devicename"]; // "12345678901234567890123456789012"
+  int relay1pin = doc["relay1pin"]; // 13
+  const char* relay1name = doc["relay1Name"]; // "12345678901234567890123456789012"
+  bool relay2enabled = doc["relay2Enabled"]; // true
+  int relay2pin = doc["relay2pin"]; // 18
+  const char* relay2name = doc["relay2Name"]; // "12345678901234567890123456789012"
 
-  char name[64];
-  strcpy(name, settingsDoc["devicename"]);
-  deviceName = name;
-  Serial.print("Device Name: "); Serial.println(deviceName);
+  deviceName = devicename;
+  relay1Pin = relay1pin;
+  relay1Name = relay1name;
+
+  relay2Enabled = relay2enabled;
+  if (relay2Enabled) {
+    relay2Pin = relay2pin;
+    relay2Name = relay2name;
+  }
+  
+  Serial.print("Device name: "); Serial.println(deviceName);
+  Serial.print("Relay 1: "); Serial.print(relay1Name); Serial.print(" on pin: "); Serial.println(relay1Pin);
+
+  Serial.print("Relay 2 enabled: "); Serial.println(relay2Enabled);
+  if (relay2Enabled) {
+    Serial.print("Relay 2: "); Serial.print(relay2Name); Serial.print(" on pin: "); Serial.println(relay2Pin);
+  }
   
   settingsFile.close();
 }
@@ -178,6 +296,17 @@ void setup() {
   connectToWifi();
   configureUrlRoutes();
   configureOTA();
+
+  Serial.print("relay1Pin name: "); Serial.println(relay1Pin);
+  Serial.print("relay2Pin name: "); Serial.println(relay2Pin);
+
+  pinMode(relay1Pin, OUTPUT);
+  digitalWrite(relay1Pin, HIGH);
+
+  if (relay2Enabled) {
+    pinMode(relay2Pin, OUTPUT);
+    digitalWrite(relay2Pin, HIGH);
+  }
   
   server.begin();
 }
